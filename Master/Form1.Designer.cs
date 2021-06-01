010f62b5cf7c799883b40aba6c6447cc357b4031
PC <lsongulija@gmail.com>
2021-06-01 18:44:12 +0200

    Master script

---
+++ Master/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Master
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SelectFolder = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TrySendingFiles = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pasirinkti paveikslėlių katalogą:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(439, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // SelectFolder
+            // 
+            this.SelectFolder.Location = new System.Drawing.Point(381, 62);
+            this.SelectFolder.Name = "SelectFolder";
+            this.SelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.SelectFolder.TabIndex = 2;
+            this.SelectFolder.Text = "Naršyti";
+            this.SelectFolder.UseVisualStyleBackColor = true;
+            this.SelectFolder.Click += new System.EventHandler(this.SelectFolder_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 119);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Size = new System.Drawing.Size(439, 300);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nr.";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Paveikslėlis";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Būsena";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 60;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pasirinkti paveikslėlių katalogą:";
+            // 
+            // TrySendingFiles
+            // 
+            this.TrySendingFiles.Location = new System.Drawing.Point(381, 425);
+            this.TrySendingFiles.Name = "TrySendingFiles";
+            this.TrySendingFiles.Size = new System.Drawing.Size(75, 23);
+            this.TrySendingFiles.TabIndex = 5;
+            this.TrySendingFiles.Text = "Apdoroti";
+            this.TrySendingFiles.UseVisualStyleBackColor = true;
+            this.TrySendingFiles.Click += new System.EventHandler(this.TrySendingFiles_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 454);
+            this.progressBar1.Maximum = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(439, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(13, 430);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Programa sustabdyta.";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(471, 493);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.TrySendingFiles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.SelectFolder);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Master";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+
+
+
+
+        
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SelectFolder;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button TrySendingFiles;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Master/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Master
+{
+    public partial class Form1 : Form
+    {
+        private List<string> images = new List<string>();
+        private List<int> imageStatus = new List<int>();
+        private List<int> ports = new List<int>() { 2020, 2030, 2040, 2050 };
+        private List<bool> portStatus = new List<bool>() { false, false, false, false };
+        private TcpListener listener = new TcpListener(IPAddress.Any, 9999);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[1].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+        private void SelectFolder_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Programa sustabdyta.";
+            progressBar1.Value = 0;
+
+            images.Clear();
+            imageStatus.Clear();
+            for (int i = 0; i < portStatus.Count; i++)
+            {
+                portStatus[i] = false;
+            }
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string selectedPath = folderBrowserDialog1.SelectedPath;
+                    string[] temp = Directory.GetFiles(selectedPath, "*.jpg");
+                    foreach (var str in temp)
+                    {
+                        images.Add(str);
+                        imageStatus.Add(0);
+                    }
+                    textBox1.Text = selectedPath;
+
+                    progressBar1.Maximum = images.Count;
+
+                    var t0 = new Thread(() =>
+                    {
+                        UpdateDatagrid();
+                    });
+                    t0.Start();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Exception while selecting category, details: " + exc);
+                }
+            }
+        }
+
+        private void UpdateDatagrid()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                try
+                {
+                    dataGridView1.Invoke((MethodInvoker)delegate
+                    {
+                        int saveRow = 0;
+                        if (dataGridView1.Rows.Count > 0 && dataGridView1.FirstDisplayedCell != null)
+                            saveRow = dataGridView1.FirstDisplayedCell.RowIndex;
+
+                        dataGridView1.Rows.Clear();
+
+                        for (int i = 0; i < images.Count; i++)
+                        {
+                            i = dataGridView1.Rows.Add();
+                            dataGridView1.Rows[i].Cells["Column1"].Value = i;
+                            dataGridView1.Rows[i].Cells["Column2"].Value = images[i];
+                            dataGridView1.Rows[i].Cells["Column3"].Value = imageStatus[i];
+                        }
+
+                        if (saveRow != 0 && saveRow < dataGridView1.Rows.Count)
+                            dataGridView1.FirstDisplayedScrollingRowIndex = saveRow;
+                    });
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Exception while updating the datagrid, details: " + exc);
+                }
+            }
+        }
+
+        private void TrySendingFiles_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Programa veikia...";
+
+            var t1 = new Thread(() =>
+            {
+                ListenForResults();
+            });
+            t1.Start();
+
+            var t2 = new Thread(() =>
+            {
+                ProgressUpdate();
+            });
+            t2.Start();
+
+            var t3 = new Thread(() =>
+            {
+                ConnectionLostCheck();
+            });
+            t3.Start();
+
+            var t4 = new Thread(() =>
+            {
+                IsFileStillAvailable();
+            });
+            t4.Start();
+
+            if (images.Count == 0)
+            {
+                MessageBox.Show("Paveikslėlių sąrašas yra tuščias!");
+            }
+            else
+            {
+                var t5 = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        for (int i = 0; i < ports.Count; i++)
+                        {
+                            if (!portStatus[i])
+                            {
+                                int limitFiveFiles = 0;
+
+                                for (int j = 0; j < images.Count; j++)
+                                {
+                                    if (limitFiveFiles == 5)
+                                        break;
+
+                                    if (imageStatus[j] == 0)
+                                    {
+                                        limitFiveFiles++;
+
+                                        try
+                                        {
+                                            IPAddress ip = IPAddress.Parse("127.0.0.1");
+                                            IPEndPoint end = new IPEndPoint(ip, ports[i]);
+                                            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+                                            byte[] fileNameBytes = Encoding.ASCII.GetBytes(Path.GetFileName(images[j]));
+                                            byte[] fileNameLen = BitConverter.GetBytes(Path.GetFileName(images[j]).Length);
+                                            byte[] preBuff = new byte[4 + fileNameBytes.Length];
+
+                                            fileNameLen.CopyTo(preBuff, 0);
+                                            fileNameBytes.CopyTo(preBuff, 4);
+
+                                            sock.Connect(end);
+                                            portStatus[i] = true;
+                                            if (File.Exists(images[j]))
+                                                sock.SendFile(images[j], preBuff, null, TransmitFileOptions.UseDefaultWorkerThread);
+                                            sock.Close();
+                                        }
+                                        catch (SocketException exc)
+                                        {
+                                            portStatus[i] = false;
+                                            break;
+                                        }
+
+                                        imageStatus[j] = ports[i];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+                t5.Start();
+            }
+        }
+
+        public void IsFileStillAvailable()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                string selectedPath = folderBrowserDialog1.SelectedPath;
+                List<string> temp = new List<String>(Directory.GetFiles(selectedPath, "*.jpg"));
+
+                for (int i = 0; i < images.Count; i++)
+                    if (!temp.Contains(images[i]))
+                    {
+                        images.RemoveAt(i);
+                        imageStatus.RemoveAt(i);
+                        break;
+                    }
+            }
+        }
+
+        public void ListenForResults()
+        {
+            listener.Start();
+
+            while (true)
+            {
+                using (var client = listener.AcceptTcpClient())
+                using (var stream = client.GetStream())
+                {
+                    byte[] fileNameLengthBytes = new byte[4];
+                    stream.Read(fileNameLengthBytes, 0, 4);
+                    int fileNameLength = BitConverter.ToInt32(fileNameLengthBytes, 0);
+                    byte[] fileNameBytes = new byte[fileNameLength];
+                    stream.Read(fileNameBytes, 0, fileNameLength);
+                    string fileName = Encoding.ASCII.GetString(fileNameBytes, 0, fileNameLength);
+
+                    using (var output = File.Create(fileName))
+                    {
+                        var buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            output.Write(buffer, 0, bytesRead);
+                        }
+                    }
+
+                    for (int i = 0; i < images.Count; i++)
+                        if (Path.GetFileNameWithoutExtension(images[i]) == Path.GetFileNameWithoutExtension(fileName))
+                            imageStatus[i] = 1;
+
+                    for (int i = 0; i < ports.Count; i++)
+                        if (!imageStatus.Contains(ports[i]) && portStatus[i] == true)
+                            portStatus[i] = false;
+                }
+            }
+        }
+
+        public void ProgressUpdate()
+        {
+            while (true)
+            {
+                int count = 0;
+                for (int i = 0; i < images.Count; i++)
+                    if (imageStatus[i] == 1)
+                        count++;
+
+                if (count == images.Count)
+                {
+                    label3.Invoke((MethodInvoker)delegate
+                    {
+                        label3.Text = "Darbas baigtas!";
+                    });
+                    CombineResults();
+                }
+
+                progressBar1.Invoke((MethodInvoker)delegate
+                {
+                    progressBar1.Value = count;
+                });
+            }
+        }
+
+        public void ConnectionLostCheck()
+        {
+            while (true)
+            {
+                for (int i = 0; i < ports.Count; i++)
+                {
+                    if (portStatus[i] == true)
+                    {
+                        try
+                        {
+                            List<int> placement = new List<int>();
+                            int count = 0;
+
+                            for (int j = 0; j < images.Count; j++)
+                                if (imageStatus[j] == ports[i])
+                                    placement.Add(j);
+
+                            Thread.Sleep(10000);
+
+                            count = 0;
+                            for (int j = 0; j < placement.Count; j++)
+                            {
+                                int temp = placement[j];
+                                if (imageStatus[placement[j]] == ports[i])
+                                    count++;
+                            }
+
+                            if (count == placement.Count && count != 0)
+                            {
+                                for (int j = 0; j < placement.Count; j++)
+                                    imageStatus[placement[j]] = 0;
+                                portStatus[i] = false;
+                            }
+                        }
+                        catch (Exception exc)
+                        {
+                            MessageBox.Show("Exception while checking if connection is lost, details: " + exc);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void CombineResults()
+        {
+            Thread.Sleep(500);
+
+            try
+            {
+                string mainDir = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+                if (File.Exists(mainDir + $@"\result.txt"))
+                    File.Delete(mainDir + $@"\result.txt");
+
+                List<string> filesIncluded = new List<string>(Directory.GetFiles(mainDir, "*.txt"));
+
+                using (var outputStream = File.Create(mainDir + $@"\result.txt"))
+                    foreach (var inputFilePath in filesIncluded)
+                    {
+                        using (var inputStream = File.OpenRead(inputFilePath))
+                            inputStream.CopyTo(outputStream);
+                        //File.Delete(inputFilePath);
+                    }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Exception while combining the results, details: " + exc);
+            }
+        }
+    }
+}
